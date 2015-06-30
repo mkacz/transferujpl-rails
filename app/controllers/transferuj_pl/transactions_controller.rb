3b47cbd4a3a1dc854c3772401a7069f4ce04125d
Marcin Lewandowski <marcin@saepia.net>
2015-07-01 00:41:53 +0200

    Check remote IP in TransferujPL::TrasactionsController

--- app/controllers/transferuj_pl/transactions_controller.rb
+++ app/controllers/transferuj_pl/transactions_controller.rb
@@ -2,6 +2,14 @@ class TransferujPL::TrasactionsController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
   def notification
+    if not Rails.env.development? and not Rails.env.test?
+      unless TransferujPL.transaction_notifications_ips.include? request.remote_ip
+        render nothing: "Request from forbidden IP", status: :forbidden 
+        return  
+      end
+    end
+
+
     render text: "TRUE"
   end
 end
